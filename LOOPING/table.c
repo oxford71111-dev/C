0899f7b5d6629ec85be80d83f7346b45cf37be54
oxford71111-dev <oxford71111@gmail.com>
2025-12-12 14:51:35 +0545

    table

---
+++ LOOPING/table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>   
+#include <conio.h>    
+int main()
+{
+     int a;
+     printf("Hello C Programming!");
+     scanf("%d",&a)
+               
+     int i;
+     for (i=1;i<=10;i++)
+     {
+          printf("\n%d",a);
+          a=a+a
+
+      }
+
+
+    getch();         
+    return 0;      
+}
